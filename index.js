f983d440581a067d6330be13c2442f5d3bc04eda
Emily Oriel <58639088+emilyoriel@users.noreply.github.com>
2020-05-10 19:39:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let herName = prompt("what is your crush name?");
 var loveScore= Math.random() *40;
     loveScore = Math.floor(loveScore);
 
-    if (herName === "emily"|| herName === "eMILY"||herName === "EMILY" || herName === "em" || herName === "e" || herName === "E" || herName === "emily oriel" || herName === "Emily Oriel" || herName === "EMILY ORIEL") {
+    if (herName === "emily"|| herName === "eMILY"||herName === "EMILY" || herName === "Emily" || herName === "emily oriel" || herName === "Emily Oriel" || herName === "EMILY ORIEL") {
         alert(yourName + ", Your love competability with " + herName + " is as strong as Kanye's love to Kanye! You guys should 100% go for it ❤️");
     } else {
         alert(yourName + ", Your love competability with " + herName + " is " + loveScore +"% NO BUENO!");
