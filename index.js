3e8d597f2dd27286fadd3eb469622f998da0082b
Emily Oriel <58639088+emilyoriel@users.noreply.github.com>
2020-05-10 19:32:05 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let yourName = prompt("What is your name?");
+let herName = prompt("what is your crush name?");
+var loveScore= Math.random() *40;
+    loveScore = Math.floor(loveScore);
+
+    if (herName === "emily"|| herName === "eMILY"||herName === "EMILY" || herName === "em" || herName === "e" || herName === "E" || herName === "emily oriel" || herName === "Emily Oriel" || herName === "EMILY ORIEL") {
+        alert(yourName + ", Your love competability with " + herName + " is as strong as Kanye's love to Kanye! You guys should 100% go for it ❤️");
+    } else {
+        alert(yourName + ", Your love competability with " + herName + " is " + loveScore +"% NO BUENO!");
+    }
